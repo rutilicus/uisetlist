c4d78d58c7015ced64a7710f6127609b58b74001
rutilicus <rutilicus.growup@gmail.com>
2020-06-02 00:43:29 +0900

    歌一覧ページをSPAっぽくする

--- src/main/kotlin/com/rutilicus/uisetlist/controller/SongController.kt
+++ src/main/kotlin/com/rutilicus/uisetlist/controller/SongController.kt
@@ -1,17 +1,28 @@
 package com.rutilicus.uisetlist.controller
 
+import com.rutilicus.uisetlist.ResourceNotFoundException
+import com.rutilicus.uisetlist.service.MovieService
 import com.rutilicus.uisetlist.service.SongService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 
 @Controller
 @RequestMapping("song")
-class SongController(val service: SongService) {
+class SongController(val songService: SongService, val movieService: MovieService) {
     @GetMapping("/")
-    fun songPage(model: Model): String {
-        model.addAttribute("songs", service.findAll())
-        return "song"
+    fun songPage(
+            @RequestParam(name = "id", required = false, defaultValue = "") id: String,
+            @RequestParam(name = "time", required = false, defaultValue = "") time: String,
+            model: Model): String {
+        if (id.isNotEmpty()) {
+            movieService.findAllByMovieId(id).getOrNull(0) ?: throw ResourceNotFoundException()
+        }
+        model.addAttribute("songs", songService.findAll())
+        model.addAttribute("id", id)
+        model.addAttribute("time", time.toIntOrNull())
+        return "songApp"
     }
 }
